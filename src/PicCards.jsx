1f9f2b948617dbfd6891b2b52248ff02a6be840c
George Tzinetas <krimini.kriminals@gmail.com>
2020-05-01 11:58:09 +0300

    Added api keys to external file or env variables

--- src/PicCards.jsx
+++ src/PicCards.jsx
@@ -1,42 +1,40 @@
-import React, { useState, useEffect } from "react";
-import Unsplash, { toJson } from "unsplash-js";
-import PicCard from "./PicCard";
+import React, { useState, useEffect } from 'react';
+import Unsplash, { toJson } from 'unsplash-js';
+import PicCard from './PicCard';
+import { config } from './config';
+import { Col, Row } from 'antd';
 
-import { Col, Row } from "antd";
-
+const ACCESSKEY = process.env.ACCESSKEY || config.ACCESSKEY;
+const SECRET = process.env.SECRET || config.SECRET;
 const unsplash = new Unsplash({
-  accessKey: "F86NAJfdsMFR4psuA8QXO_0l5lHgZz8pmUhkIqzwsco",
-  secret: "h6uVIo66IwUa7S7xgi2NBsfTfePnhjgQz07VO0zTWbg",
+  accessKey: ACCESSKEY,
+  secret: SECRET,
 });
 
-function PicCards({keyword}) {
+function PicCards({ keyword }) {
   const [pic, setPic] = useState([]);
 
   useEffect(() => {
-    unsplash
-      .search.photos(keyword, 1, 4, { orientation: "portrait" })
+    unsplash.search
+      .photos(keyword, 1, 4, { orientation: 'portrait' })
       .then(toJson)
       .then((json) => {
-        console.log("inside promise");
+        console.log('inside promise');
         console.log(keyword);
         console.log(json);
         const pics = json.results.map((p) => p.urls.small);
         setPic(pics);
-        
       });
-  }, [{keyword}]); // Only re-subscribe if props.friend.id changes
+  }, [{ keyword }]); // Only re-subscribe if props.friend.id changes
 
   return (
     <div>
-      <Row gutter={16} style={{marginTop: 16}}>  
-
-      {pic.map((pic, idx) => (
-      <Col span={6}>
-        <PicCard key={idx} pic = {pic} />
-      </Col>
-      ))}
-
-      
+      <Row gutter={16} style={{ marginTop: 16 }}>
+        {pic.map((pic, idx) => (
+          <Col span={6}>
+            <PicCard key={idx} pic={pic} />
+          </Col>
+        ))}
       </Row>
     </div>
   );
