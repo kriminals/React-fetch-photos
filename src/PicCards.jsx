f191ce0f513b93626a80433b2aa67f8e905f8d5e
George Tzinetas <krimini.kriminals@gmail.com>
2020-05-05 09:40:44 +0300

    Used conditional import and env variables

--- src/PicCards.jsx
+++ src/PicCards.jsx
@@ -1,31 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import Unsplash, { toJson } from 'unsplash-js';
 import PicCard from './PicCard';
-import { config } from './config';
 import { Col, Row } from 'antd';
-
-const ACCESSKEY = process.env.ACCESSKEY || config.ACCESSKEY;
-const SECRET = process.env.SECRET || config.SECRET;
-const unsplash = new Unsplash({
-  accessKey: ACCESSKEY,
-  secret: SECRET,
-});
 
 function PicCards({ keyword }) {
   const [pic, setPic] = useState([]);
 
-  useEffect(() => {
-    unsplash.search
-      .photos(keyword, 1, 4, { orientation: 'portrait' })
-      .then(toJson)
-      .then((json) => {
-        console.log('inside promise');
-        console.log(keyword);
-        console.log(json);
-        const pics = json.results.map((p) => p.urls.small);
-        setPic(pics);
+  
+  async function connect() {
+    if (!process.env.ACCESSKEY) {
+      const { config } = await import('./config');
+      const unsplash = new Unsplash({
+        accessKey: config.ACCESSKEY,
+        secret: config.SECRET,
       });
-  }, [{ keyword }]); // Only re-subscribe if props.friend.id changes
+      return unsplash;
+    }
+    const unsplash = new Unsplash({
+      accessKey: process.env.ACCESSKEY,
+      secret: process.env.SECRET,
+    });
+    return unsplash;
+  }
+
+  const unsplash = connect();
+  useEffect(() => {
+    unsplash.then((response) =>
+      response.search
+        .photos(keyword, 1, 4, { orientation: 'portrait' })
+        .then(toJson)
+        .then((json) => {
+          const pics = json.results.map((p) => p.urls.small);
+          setPic(pics);
+        })
+    );
+  }, [{ keyword }]); // Only re-subscribe if props changes
 
   return (
     <div>
